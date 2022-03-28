38bb2a29294feb6fc866c3a296078f3a4f57274c
AlanDev <ingescobedoalan4@gmail.com>
2022-03-28 17:42:19 -0600

    Se elimino el reducer de usuarios

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit"
 import { loginApi } from "./slices/login/login"
 import loginReducer from "./slices/login"
-import usersReducer from "./slices/users"
 import notesReducer from "./slices/notes"
 
 
 export const store = configureStore({
     reducer: {
-        users: usersReducer,
         notes: notesReducer,
         login: loginReducer,
         [loginApi.reducerPath]: loginApi.reducer
