8e3b8d1e64fc0847072374ee64b990e850b9d0fe
AlanDev <ingescobedoalan4@gmail.com>
2022-03-28 20:04:53 -0600

    Se agregaron mensajes de error mas correctos

--- src/components/notes/EditNoteForm.jsx
+++ src/components/notes/EditNoteForm.jsx
@@ -1,11 +1,11 @@
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { addNote, editNote } from "../../reducers/slices/notes";
+import ErrorMessage from "../auth/ErrorMessage";
 
 const EditNoteForm = ({ setShowModal, defaultNote }) => {
   const {
     register,
-    watch,
     handleSubmit,
     formState: { errors },
   } = useForm();
@@ -16,14 +16,12 @@ const EditNoteForm = ({ setShowModal, defaultNote }) => {
   const { user } = useSelector((state) => state.login)
 
   const onSubmit = (data) => {
-    console.log(data);
     const newNote = {
         id,
         title: data.title,
         content: data.content,
         user: user.name,
     }
-    console.log(newNote)
     dispatch(editNote(newNote))
     setShowModal(false);
     };
@@ -43,18 +41,29 @@ const EditNoteForm = ({ setShowModal, defaultNote }) => {
               },
             })}
             defaultValue={title}
-            className="p-l-[17px] p-t-[14px] p-l-[12px] border border-border-gray-light rounded-lg text-[16px] text-gray-light focus:outline-none focus:ring focus:border-gray placeholder-gray-light"
+            className={
+              errors.title
+                ? "p-l-[17px] p-t-[14px] p-l-[12px] border border-red-primary rounded-lg text-[16px] text-red-primary focus:outline-none focus:border-gray placeholder-red-primary"
+                : "p-l-[17px] p-t-[14px] p-l-[12px] border border-border-gray-light rounded-lg text-[16px] text-gray-light focus:outline-none focus:border-gray placeholder-gray-light"
+            }
             type="text"
             placeholder="Ingresar Título"
           />
+          {
+            errors.title &&
+            <ErrorMessage message={errors.title.message} />
+          }
         </div>
         <div className="flex flex-col gap-2 mt-[22px] px-5">
           <label className="text-text text-primary-font-color">
             Cuerpo de la nota
           </label>
           <textarea
-            className="resize-none p-l-[17px] p-t-[14px] p-l-[12px] border border-border-gray-light rounded-lg text-[16px] text-gray-light  focus:border-gray placeholder-gray-light h-[132px]
-            "
+           className={
+            errors.content
+              ? "resize-none p-l-[17px] p-t-[14px] p-l-[12px] border border-red-primary rounded-lg text-[16px] text-red-primary  focus:border-red-primary placeholder-red-primary h-[132px]"
+              : "resize-none p-l-[17px] p-t-[14px] p-l-[12px] border border-border-gray-light rounded-lg text-[16px] text-gray-light  focus:border-gray placeholder-gray-light h-[132px]"
+          }
             {...register("content", {
               required: {
                 value: true,
@@ -64,13 +73,17 @@ const EditNoteForm = ({ setShowModal, defaultNote }) => {
             defaultValue={content}
             placeholder="Ingresa el cuerpo"
           />
+           {
+            errors.content &&
+            <ErrorMessage message={errors.content.message} />
+          }
         </div>
         <div className="flex items-center justify-end p-[14px] mt-8 border-t border-solid border-border-gray rounded-b">
           <button
             className="bg-[#3554D1] text-white text-subtitle px-[22px] py-[10px] rounded-[5px] shadow hover:shadow-lg outline-none font-medium"
             type="submit"
           >
-            Save Changes
+            Añadir Nota
           </button>
         </div>
       </form>
