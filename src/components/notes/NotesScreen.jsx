5c59569b0cc16faf5a84ad609a7b9923fc9cd3ce
AlanDev <ingescobedoalan4@gmail.com>
2022-03-31 17:43:33 -0600

    Evitando renderizado si no esta auntenticado

--- src/components/notes/NotesScreen.jsx
+++ src/components/notes/NotesScreen.jsx
@@ -1,14 +1,23 @@
+import { useSelector } from "react-redux";
 import AddNote from "./AddNote";
 import NavBar from "./NavBar";
 import NotesLayout from "./NotesLayout";
 
 const NotesScreen = () => {
-    
+  const { isAuthenticated } = useSelector(state => state.login);
   return (
     <div className="w-full h-screen p-3">
-      <NavBar />
-      <AddNote />
-      <NotesLayout />
+      
+      {
+        isAuthenticated 
+          ? (
+          <>
+            <NavBar />
+            <AddNote />
+            <NotesLayout />
+          </>
+          ) : null
+      }
     </div>
   );
 };
